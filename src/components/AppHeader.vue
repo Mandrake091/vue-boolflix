9ff201ca8dec5431865e655ecd60cafdcf730441
Mandrake091 <wasdgamer91@gmail.com>
2022-05-12 00:28:27 +0200

    fix

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,52 +1,61 @@
 <template>
-    <header class="container-fluid ">
-        <div class="row align-items-center justify-content-between h-100">
-            <div class="col-5">
-                 <img class="" src="../assets/images/Logonetflix.png" alt="">  
-            </div>
-           <div class="col-5 col-md-3">
-                <div class="input-group input-group-sm d-flex justify-content-end ">
-  <input type="text" class="form-control" placeholder="Cerca per titolo" aria-label="Recipient's username" aria-describedby="button-addon2" v-model="search" @keyup.enter="$emit('performSearch',search)">
-  <button class="btn btn-outline-secondary" type="button" id="button-addon2" @click="find">Cerca</button>
+  <header class="container">
+    <div class="row align-items-center justify-content-between h-100">
+      <div class="col-4">
+        <img class="" src="../assets/images/Logonetflix.png" alt="" />
+      </div>
+      <div class="col-8 col-md-3">
+        <div class="input-group input-group-sm d-flex justify-content-end">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Cerca per titolo"
+            aria-label="Recipient's username"
+            aria-describedby="button-addon2"
+            v-model="search"
+            @keyup.enter="$emit('performSearch', search)"
+          />
+          <button
+            class="btn btn-outline-secondary"
+            type="button"
+            id="button-addon2"
+            @click="find"
+          >
+            Cerca
+          </button>
         </div>
-        </div> 
-</div>
-
-    </header>
+      </div>
+    </div>
+  </header>
 </template>
 <script>
-
-
 export default {
-    components: {  },
-    name: "AppHeader",
-    data(){
-        return{
-            search:""
-        } 
+  components: {},
+  name: "AppHeader",
+  data() {
+    return {
+      search: "",
+    };
+  },
+  methods: {
+    find() {
+      this.$emit("performSearch", this.search);
+      this.search = "";
     },
-     methods:{      
-            find(){
-            this.$emit('performSearch', this.search)
-            this.search = ''
-        }
-    },
-    computed:{
-
-    }
-}
+  },
+  computed: {},
+};
 </script>
 
 <style lang="scss">
 @import "../style/generals.scss";
 
-img{
-    height: 40px; 
+img {
+  height: 40px;
 }
 
-header{
-    height: 80px;
-    background-color: $bg-navbar;
-    box-shadow: 5px 10px 20px #0000006b;
+header {
+  height: 80px;
+  background-color: $bg-navbar;
 }
 </style>
--- src/components/AppPopular.vue
+++ src/components/AppPopular.vue
@@ -1,101 +1,125 @@
 <template>
-<section> 
+  <section>
     <div class="container">
-        <div class="row">
-          <h1 class="p-0">Popular</h1>   
-        </div>
+      <div class="row">
+        <h1 class="">Popular</h1>
+      </div>
     </div>
-   
-    <app-loader v-if="loading"/>
-    <carousel paginationPosition="bottom-overlay" :speed="1500" :centerMode="true" :autoplay="true" :autoplayTimeout="5000" :per-page="5" class="m-auto">
-        <slide class="text-center" v-for="item in popular" :key="item.id">
-        <div class="card m-auto" style="width: 8rem;">
-  <img :src="'https://image.tmdb.org/t/p/w500/'+ item.poster_path" alt="">
-    <div class="card-body d-flex flex-column justify-content-between">
-       <p class="card-text d-flex align-items-center justify-content-center">Voto: {{item.vote_average}}
-           <country-flag :country='item.original_language === "en" ? item.original_language = "it" : item.original_language' size='small'/>
-       </p>
-  </div>
-</div>         
-</slide>
-</carousel>
-     
-        
-</section>
+
+    <app-loader v-if="loading" />
+    <carousel
+      paginationPosition="bottom-overlay"
+      :speed="1500"
+      :autoplay="true"
+      :autoplayTimeout="5000"
+      :per-page="3"
+      class=""
+    >
+      <slide class="text-center" v-for="item in popular" :key="item.id">
+        <div class="card m-auto" style="width: 8rem">
+          <img
+            :src="'https://image.tmdb.org/t/p/w500/' + item.poster_path"
+            alt=""
+          />
+          <div class="card-body d-flex flex-column justify-content-between">
+            <p
+              class="card-text d-flex align-items-center justify-content-center"
+            >
+              Voto: {{ item.vote_average }}
+              <country-flag
+                :country="
+                  item.original_language === 'en'
+                    ? (item.original_language = 'it')
+                    : item.original_language
+                "
+                size="small"
+              />
+            </p>
+          </div>
+        </div>
+      </slide>
+    </carousel>
+  </section>
 </template>
 
 <script>
-import store from '../store.js';
-import axios from 'axios';
-import CountryFlag from 'vue-country-flag'
-import { Carousel, Slide } from 'vue-carousel';
-import AppLoader from './AppLoader.vue';
+import store from "../store.js";
+import axios from "axios";
+import CountryFlag from "vue-country-flag";
+import { Carousel, Slide } from "vue-carousel";
+import AppLoader from "./AppLoader.vue";
 
 export default {
-    name: "AppPopular",
-    props:['popular'],
-    components: { AppLoader, Carousel, Slide, CountryFlag },
-    data(){
-        return {
-            lastPopular:[],
-            api:'https://api.themoviedb.org/3/search/movie?api_key=6853d87823c5e52e6b967b2482fca241&language=it-IT&query=',
-            apiKey:'6853d87823c5e52e6b967b2482fca241',
-            apiSearch:'&language=it-IT&query=',
-            apiLastPopular:'https://api.themoviedb.org/3/movie/popular?api_key=6853d87823c5e52e6b967b2482fca241&language=it-IT&page=1',
-            loading:false,
-            searchedMovies:[],
-            authors:[],
-            searchText:"",
-        }
-    },
-    methods:{
-        mySearch(text){
-            this.loading=true
-        setTimeout(()=>{
-            axios.get(this.api + store.state.search).then((res)=>{
+  name: "AppPopular",
+  props: ["popular"],
+  components: { AppLoader, Carousel, Slide, CountryFlag },
+  data() {
+    return {
+      lastPopular: [],
+      api: "https://api.themoviedb.org/3/search/movie?api_key=6853d87823c5e52e6b967b2482fca241&language=it-IT&query=",
+      apiKey: "6853d87823c5e52e6b967b2482fca241",
+      apiSearch: "&language=it-IT&query=",
+      apiLastPopular:
+        "https://api.themoviedb.org/3/movie/popular?api_key=6853d87823c5e52e6b967b2482fca241&language=it-IT&page=1",
+      loading: false,
+      searchedMovies: [],
+      authors: [],
+      searchText: "",
+    };
+  },
+  methods: {
+    mySearch(text) {
+      this.loading = true;
+      setTimeout(() => {
+        axios
+          .get(this.api + store.state.search)
+          .then((res) => {
             this.searchedMovies = res.data.results;
-            console.log(this.albumList)
-            console.log(this.authors)
-        }).catch((error) => {
-            console.log(error)
-        })
-               this.searchText = text; 
-               this.loading=false
-            },1000)
-        },
-        myfind(){
-            console.log(store.state.search)
-            return store.state.searchText
-        }
+            console.log(this.albumList);
+            console.log(this.authors);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        this.searchText = text;
+        this.loading = false;
+      }, 1000);
     },
-       computed:{
-       },
-    created(){
-        this.loading = true;
-        setTimeout(()=>{
-            this.loading = false,
-            axios.get(this.apiLastPopular).then((res)=>{
+    myfind() {
+      console.log(store.state.search);
+      return store.state.searchText;
+    },
+  },
+  computed: {},
+  created() {
+    this.loading = true;
+    setTimeout(() => {
+      (this.loading = false),
+        axios
+          .get(this.apiLastPopular)
+          .then((res) => {
             this.lastPopular = res.data.results;
-            console.log(this.albumList)
-            console.log(this.genre)
-            console.log(this.authors)
-        }).catch((error) => {
-            console.log(error)
-        }) 
-        },2000)
-    },    
-}
+            console.log(this.albumList);
+            console.log(this.genre);
+            console.log(this.authors);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    }, 2000);
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-h1{
-    color: antiquewhite;
+h1 {
+  color: antiquewhite;
 }
-.card{
-    min-height: 200pxpx;
-    font-size: 0.8em;
-    }
-   img{
-       height: 150px
-    }
+.card {
+  min-height: 200pxpx;
+  font-size: 0.8em;
+}
+img {
+  height: 150px;
+}
 </style>
